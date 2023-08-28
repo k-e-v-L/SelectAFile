872cb093f61787b76b4a02eff87e808204241500
M Kael <kevliiver@gmail.com>
2023-08-28 19:29:31 +0300

    Can open now JFrame and click on button

---
+++ MyFrame.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyFrame extends JFrame implements ActionListener {
+
+    JButton button;
+    MyFrame(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new FlowLayout());
+
+        button = new JButton("Select file");
+        button.addActionListener(this);
+
+        this.add(button);
+        this.pack();
+        this.setVisible(true);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource()==button) {
+            JFileChooser fileChooser = new JFileChooser();
+        }
+
+    }
+}
